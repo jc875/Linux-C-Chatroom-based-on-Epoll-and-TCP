6269031d81ae8a73389fb64f9c78b2f7c9498dc0
Jing Cheng <34455602@qq.com>
2026-06-09 21:33:42 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,23 +1,31 @@
-#include "mainwindow.h"
+#include "ChatServer.h"
 
-#include <QApplication>
-#include <QLocale>
-#include <QTranslator>
+#include <cstdlib>
+#include <iostream>
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-    QApplication a(argc, argv);
+    // 默认监听 8888 端口；运行时也可以通过命令行参数指定端口。
+    int port = 8888;
 
-    QTranslator translator;
-    const QStringList uiLanguages = QLocale::system().uiLanguages();
-    for (const QString &locale : uiLanguages) {
-        const QString baseName = "ChatClient_" + QLocale(locale).name();
-        if (translator.load(":/i18n/" + baseName)) {
-            a.installTranslator(&translator);
-            break;
-        }
+    if (argc >= 2)
+    {
+        port = std::atoi(argv[1]);
     }
-    MainWindow w;
-    w.show();
-    return QApplication::exec();
+
+    // main 只负责启动服务器，具体的 socket 和 epoll 逻辑都封装在 ChatServer 中。
+    ChatServer server;
+
+    if (!server.Init(port))
+    {
+        std::cerr << "Server init failed." << std::endl;
+        return 1;
+    }
+
+    std::cout << "Chat server is running on port " << port << std::endl;
+
+    // 进入 epoll 主循环后，服务器会一直等待并处理客户端事件。
+    server.Run();
+
+    return 0;
 }
